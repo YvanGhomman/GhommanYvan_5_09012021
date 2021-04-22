719603ecc255589f26282de441c0fe3895b1b825
YvanGhomman <yvan.ghomman.yg@gmail.com>
2021-04-22 12:38:06 +0200

    Add all products

--- appe.js
+++ appe.js
@@ -1,58 +1,131 @@
-/*let httpRequest = new XMLHttpRequest()
-
-httpRequest.onreadystatechange = function () {
-console.log(httpRequest)
-}
-
-httpRequest.open('GET', 'http://localhost:3000/api/furniture', true)
-httpRequest.send()*/
-
-
-
 /* MISE EN ROUTE SERVEUR 3000 :
 PS C:\Users\yvang\Desktop\OpenClassrooms\P5\Orinoco> cd .\back-end\
 PS C:\Users\yvang\Desktop\OpenClassrooms\P5\Orinoco\back-end> node .\server.js */
 
 
 const ALL_DATA_ENDPOINT = 'http://localhost:3000/api/furniture';
-  
+const furniture1 = {
+    adress : 'http://localhost:3000/api/furniture/5be9cc611c9d440000c1421e',
+    htmlId : document.getElementById("Produit1")
+};
+const furniture2 = {
+    adress :'http://localhost:3000/api/furniture/5beaadda1c9d440000a57d98' ,
+    htmlId :document.getElementById("Produit2")
+};
+const furniture3 = {
+    adress :'http://localhost:3000/api/furniture/5beaae361c9d440000a57d99' ,
+    htmlId :document.getElementById("Produit3")
+};
+const furniture4 = {
+    adress : 'http://localhost:3000/api/furniture/5beaaf2e1c9d440000a57d9a',
+    htmlId :document.getElementById("Produit4")
+};
+const furniture5 = {
+    adress :'http://localhost:3000/api/furniture/5beab2061c9d440000a57d9b' ,
+    htmlId :document.getElementById("Produit5")
+};
+
+
 // Get Function
 async function getData(endpoint) {
     let response = await fetch(endpoint);
     return await response.json();
-}
+};
 
-getData(ALL_DATA_ENDPOINT + '/5be9cc611c9d440000c1421e')
-    .then(data => {
-         console.log(data);
-         displayProduct(data);
-         })
-    .catch(error => console.error(error))
+function template(product) {
+    return `<div class="card" style="width: 18rem;">
+                     <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}">
+                     <div class="card-body">
+                     <h5 class="card-title">${product.name}</h5>
+                     <p class="card-text">${product.description}</p>
+                     <p class="card-text">${product.price} Dogecoins</p>
+                     <a href="#" class="btn btn-primary">Kilébo ! Jeleveu ♥</a>
+                     </div>
+                 </div>`;
+};
+
+/*
+function Go(data){
+getData(data.adress)
+.then(data => {
+    function displayProduct(data) {
+        data.htmlId.innerHTML += template(data);
+    }
+        displayProduct(data);
+        })
+.catch(error => console.error(error));
+};
+
+Go(furniture1);
+Go(furniture2);
+Go(furniture3);
+Go(furniture4);
+Go(furniture5);
+*/
+
+
+getData(furniture1.adress)
+.then(data => {
+    function displayProduct(data) {
+        furniture1.htmlId.innerHTML += template(data);
+    }
+        displayProduct(data);
+        })
+.catch(error => console.error(error));
+
+
+getData(furniture2.adress)
+.then(data => {
+    function displayProduct(data) {
+        furniture2.htmlId.innerHTML += template(data);
+    }
+        displayProduct(data);
+        })
+.catch(error => console.error(error));
+
+
+getData(furniture3.adress)
+.then(data => {
+    function displayProduct(data) {
+        furniture3.htmlId.innerHTML += template(data);
+    }
+        displayProduct(data);
+        })
+.catch(error => console.error(error));
+
+
+getData(furniture4.adress)
+.then(data => {
+    function displayProduct(data) {
+        furniture4.htmlId.innerHTML += template(data);
+    }
+        displayProduct(data);
+        })
+.catch(error => console.error(error));
+
+
+getData(furniture5.adress)
+.then(data => {
+    function displayProduct(data) {
+        furniture5.htmlId.innerHTML += template(data);
+    }
+        displayProduct(data);
+        })
+.catch(error => console.error(error));
 
 
 
+/*
 // Display Products
 function displayProduct(product) {
     console.log(product);
-    let container = document.getElementById("allProducts");
+    let container = document.getElementById("Produit1");
     console.log(container);
-    /*container.innerHTML += '<div class="card" style="width: 18rem;">';
-    container.innerHTML += ` <img src=${product.imageUrl} class="card-img-top" alt="${product.name}">`;
-    container.innerHTML += '<div class="card-body">';
-    container.innerHTML += '<h5 class="card-title">'+ product.name + '</h5>';
-    container.innerHTML += '<p class="card-text">' +product.description  + '</p>';
-    container.innerHTML += '<a href="#" class="btn btn-primary">Go somewhere</a>';
-    container.innerHTML += '</div>';
-    container.innerHTML += '</div>';*/
+    container.innerHTML += template(product)
+}*/
 
-   let template = `<div class="card" style="width: 18rem;">
-                        <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}">
-                        <div class="card-body">
-                        <h5 class="card-title">${product.name}</h5>
-                        <p class="card-text">${product.description}</p>
-                        <p class="card-text">${product.price} Dogecoins</p>
-                        <a href="#" class="btn btn-primary">Ce meuble est cool ! Jeleveu</a>
-                        </div>
-                    </div>`;
-    container.innerHTML += template
-}
+
+
+
+
+
